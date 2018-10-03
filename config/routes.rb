b06e3241187c96d7eb0da2fd8f41500657b1b0ce
Lance Dennis <golem91488@gmail.com>
2018-10-03 08:13:18 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'league/index'
-  resources :league
+  resources :leagues do
+      resources :bowlers
+      resources :weeks
+      resources :teams
+  end
   root 'league#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
